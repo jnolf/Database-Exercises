55b00d1d23c26754bcd85e7d536298a14f3a6f45
Jerry Nolf <nolf.jerry@gmail.com>
2022-01-13 15:28:12 -0600

    updated

--- case_exercises.sql
+++ case_exercises.sql
@@ -42,12 +42,68 @@ END AS Department_Group, ROUND(AVG(salary)) AS Average_Salary
 FROM departments d
 JOIN dept_emp de USING (dept_no)
 JOIN salaries s USING (emp_no)
-WHERE s.to_date > NOW()
+WHERE s.to_date > NOW() AND de.to_date > NOW()
 GROUP BY Department_Group
 ORDER BY Average_Salary;
 
 
+------- More Exercises
+
+#1
+USE sakila;
+
+-- a.
 SELECT *
-FROM departments;
+FROM actor;
+
+-- b.
+SELECT last_name
+FROM actor;
+
+-- c.
+SELECT film_id, title, release_year
+FROM film;
+
+
+#2a.
+SELECT DISTINCT last_name
+FROM actor;
+
+-- b.
+SELECT DISTINCT postal_code
+FROM address;
+
+SELECT DISTINCT rating
+FROM film;
+
+
+#3a.
+SELECT title, description, rating, length
+FROM film
+WHERE length > 180;
+
+-- b.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date > '2005-05-27'
+ORDER BY payment_date;
+
+-- c.
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE payment_date LIKE '2005-05-27%';
+
+-- d.
+SELECT *
+FROM customer
+WHERE last_name LIKE 's%' AND first_name LIKE '%n';
+
+-- e.
+SELECT *
+FROM customer
+WHERE active = '0' OR last_name LIKE 'M%';
+
+SELECT *
+FROM customer;
 
 
